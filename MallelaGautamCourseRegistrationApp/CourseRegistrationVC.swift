9f3f5191f8dd612fabeabb1d10b4cd1f691fc645
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-02-06 19:58:58 -0600

    working app with all  code changes

--- MallelaGautamCourseRegistrationApp/CourseRegistrationVC.swift
+++ MallelaGautamCourseRegistrationApp/CourseRegistrationVC.swift
@@ -18,16 +18,12 @@ class CourseRegistrationVC: UIViewController {
     @IBOutlet weak var messageLBL: UILabel!
     
     //two feilds to take user first name and last name
-    
-    
     @IBOutlet weak var firstNameTF: UITextField!
-    
-   
     @IBOutlet weak var lastNameTF: UITextField!
     
     //3 switches to select the courses
     @IBOutlet weak var iOSSWCH: UISwitch!
-    @IBOutlet weak var courseSWCH: UISwitch!
+    @IBOutlet weak var bigDataSWCH: UISwitch!
     @IBOutlet weak var patternsSWCH: UISwitch!
     
     @IBAction func toggleiOS(_ sender: UISwitch) {
@@ -49,7 +45,7 @@ class CourseRegistrationVC: UIViewController {
         if(firstName == "" || lastName == ""){
             self.messageLBL.text = "Please enter your full name."
         }
-        else if ((self.iOSSWCH.isOn == true || self.patternsSWCH.isOn == true  || self.courseSWCH.isOn == true) && (self.firstNameTF.text != "" && self.lastNameTF.text != "") ){
+        else if ((self.iOSSWCH.isOn == true || self.patternsSWCH.isOn == true  || self.bigDataSWCH.isOn == true) && (self.firstNameTF.text != "" && self.lastNameTF.text != "") ){
             
             if(self.iOSSWCH.isOn == true ){
                 count += 1
@@ -59,7 +55,7 @@ class CourseRegistrationVC: UIViewController {
                 count += 1
                 courses += "patterns, "
             }
-            if(self.courseSWCH.isOn == true ){
+            if(self.bigDataSWCH.isOn == true ){
                 count += 1
                 courses += "bigdata."
             }
@@ -79,9 +75,7 @@ class CourseRegistrationVC: UIViewController {
         self.messageLBL.text = ""
         self.iOSSWCH.isOn = false
         self.patternsSWCH.isOn = false
-        self.courseSWCH.isOn = false
-        
-        
+        self.bigDataSWCH.isOn = false
     }
     
 }
